2c206f89ca799843e1a7a91ff0e5ebf549e1d961
Sebastian L <99sebastianl@gmail.com>
2025-06-01 18:16:50 -0400

    Updates

---
+++ static/js/952.f3cb2314.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkvocal_coach_site=self.webpackChunkvocal_coach_site||[]).push([[952],{952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.f3cb2314.chunk.js.map
